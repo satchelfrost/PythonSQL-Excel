9294a19f8fb547fdb438803382ccb50f7c6eddad
Gallagher <reese.a.gallagher@accenture.com>
2020-06-09 13:17:47 -0400

    Add query from separate file

--- test.py
+++ test.py
@@ -7,14 +7,21 @@ pswd = getpass("Password:")
 # Connect to database
 db = ms.connect(host="localhost", user="root", passwd=pswd, db="gregs_list")
 
-# Create a query to return from our database
-cur = db.cursor()
-cur.execute("SELECT * FROM easy_drinks where main = 'soda'")
+# Select a query from a file
+q = open('Query_00.txt', 'r')
+lines = q.readlines()
+query = ""
+for line in lines:
+	query += line 
 
-# Print out a table from our query
+# Execute a query to return from our database
+cur = db.cursor()
+cur.execute(query)
+
+## Print out a table from our query
 print("")
 for row in cur.fetchall():
-	print(row[0])
+        print(row[0])
 
 # Close out of our database
 db.close()
