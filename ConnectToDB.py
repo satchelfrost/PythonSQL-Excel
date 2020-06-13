b2160381485d6605350bd8e6669aacc01a2b8725
Gallagher <reese.a.gallagher@accenture.com>
2020-06-12 22:10:08 -0400

    rebuild files as classes

--- ConnectToDB.py
+++ ConnectToDB.py
@@ -1,28 +1,33 @@
 import MySQLdb as ms
 from getpass import getpass
 
-# Get password for database but don't display on screen
-pswd = getpass("Password:")
+class DB:
+    def __init__(self):
+        # Get password for database but don't display on screen
+        pswd = getpass("Password:")
+        # Connect to database
+        self.db = ms.connect(host="localhost", user="root", passwd=pswd, db="gregs_list")
 
-# Connect to database
-db = ms.connect(host="localhost", user="root", passwd=pswd, db="gregs_list")
+    def RunQuery(self, query):
+        # Select a query from a file
+        # Execute a query to return from our database
+        self.cur = self.db.cursor()
+        self.cur.execute(query)
 
-# Select a query from a file
-q = open('Query_00.txt', 'r')
-lines = q.readlines()
-query = ""
-for line in lines:
-	query += line 
+    def RunQueryFromFile(self, fileName):
+        file = open(fileName, 'r')
+        lines = file.readlines()
+        query = ""
+        for line in lines:
+            query += line
+        self.RunQuery(query)
 
-# Execute a query to return from our database
-cur = db.cursor()
-cur.execute(query)
+    def PrintResult(self):
+        ## Print out a table from our query
+        print("")
+        for row in self.cur.fetchall():
+            print(row[0])
 
-## Print out a table from our query
-print("")
-for row in cur.fetchall():
-        print(row[0])
-
-# Close out of our database
-db.close()
-
+    def __del__(self):
+        # Close out of our database
+        self.db.close()
--- ConnectToExcel.py
+++ ConnectToExcel.py
@@ -1,38 +1,81 @@
 import openpyxl as opx
 from tabulate import tabulate
 
-# Load the excel file, and the specific sheet
-wb = opx.load_workbook("example.xlsx")
-
-def CreateListFromWorksheet(ws):
-    MyList = []
-    for i in range(ws.max_row-1):
-        MyList.append([])
-
-    for i in range(2, ws.max_row + 1):
-        for j in range(1, ws.max_column + 1):
-            CellValue = ws.cell(row = i, column = j).value
-            MyList[i-2].append(CellValue)
-    return MyList
-
-def CreateHeadersFromWorksheet(ws):
-    Headers = []
-    for i in range(1, ws.max_column + 1):
-        CellValue = ws.cell(row = 1, column = i).value
-        Headers.append(CellValue)
-    return Headers
-
-def PrintTableFromWorksheet(ws):
-    TableData = CreateListFromWorksheet(ws)
-    Headers = CreateHeadersFromWorksheet(ws)
-    print(tabulate(TableData, headers=Headers, tablefmt='orgtbl'))
-
-for sheet in wb.worksheets:
-    PrintTableFromWorksheet(sheet)
-    print("")
+class SpreadSheet:
     
+    def __init__(self, name):
+        # Load the excel file, and the specific sheet
+        self.wb = opx.load_workbook(name)
 
+    # This list will hold the data from the spreadsheet
+    def CreateTableDataFromWorksheet(self, ws):
+        TableData = []
+        for i in range(ws.max_row-1):
+            TableData.append([])
+            
+        for i in range(2, ws.max_row + 1):
+            for j in range(1, ws.max_column + 1):
+                CellValue = ws.cell(row = i, column = j).value
+                TableData[i-2].append(CellValue)
+        return TableData
 
-    
+    # Headers from th spreadsheet
+    def CreateHeadersFromWorksheet(self, ws):
+        Headers = []
+        for i in range(1, ws.max_column + 1):
+            CellValue = ws.cell(row = 1, column = i).value
+            Headers.append(CellValue)
+        return Headers
 
+    # Print out the table from the worksheet
+    def PrintTableFromWorksheet(self, ws):
+        TableData = self.CreateTableDataFromWorksheet(ws)
+        Headers = self.CreateHeadersFromWorksheet(ws)
+        print(tabulate(TableData, headers=Headers, tablefmt='orgtbl'))
+
+    def PrintAllTables(self):
+        for sheet in self.wb.worksheets:
+            self.PrintTableFromWorksheet(sheet)
+            print("")
+
+    def GenerateInsertQuery(self, ws, name):
+        # Insert portion of query
+        file = open("Queries/" + name + ".txt", 'w')
+        string = "INSERT INTO " + ws.title
+        file.write(string)
+        string = ""
+
+        # Header field portion of query        
+        Headers = self.CreateHeadersFromWorksheet(ws)
+        fields = "\n("
+        for i in range(len(Headers)):
+            fields += Headers[i]
+            if (i != len(Headers) - 1):
+                fields += ", "
+        string += fields + ")"
+        file.write(string)
+        string = ""
+
+        # Beginning of values portion of query
+        file.write("\nVALUES\n")
+
+        # Values portion of query
+        TableData = self.CreateTableDataFromWorksheet(ws)
+        values = ""
+        for i in range(len(TableData)):
+            value = "("
+            for j in range(len(TableData[i])):
+                value += str(TableData[i][j])
+                if (j != len(TableData[i]) - 1):
+                    value += ", "
+            value += ")"
+            if (i != len(TableData) - 1):
+                    value += ",\n"
+            values += value
+        string += values
+        file.write(string)
+        
+ss = SpreadSheet("example.xlsx")
+for sheet in ss.wb.worksheets:
+    ss.GenerateInsertQuery(sheet, sheet.title)
 
